70484a2e4472454f6b55b87f73430e1eff7677cb
Diego Vasquez <botellongo69@outlook.com>
2020-01-24 18:45:48 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,7 +1,7 @@
 def oxford_comma(array)
-  if array.lenght === 1 
+  if array.size === 1 
     return array.join()
-  elsif array.lenght === 2 
+  elsif array.size === 2 
     return array.join(' and ')
   end
 end
